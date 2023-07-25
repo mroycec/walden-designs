cceb6ac73c6181287c06c11a354fe28966f6aca6
mroycec <mroycec@icloud.com>
2023-07-25 09:41:40 -0700

    Change to client side component, add useState, replace redirect with state change

--- src/pages/pre-launch.tsx
+++ src/pages/pre-launch.tsx
@@ -1,4 +1,5 @@
-import { FormEvent, MouseEvent } from 'react'
+"use client"
+import { FormEvent, useState } from 'react'
 import Button from '../components/button/button'
 import Input from '../components/input/input'
 import { useRouter } from 'next/router'
@@ -6,9 +7,10 @@ import { useRouter } from 'next/router'
 export default function Home() {
   const router = useRouter()
   const handleSubmit = (event: FormEvent) => {
-    event.preventDefault()
-    router.push('/confirmation')
+    setIsSubmitted(true)
   }
+
+  const [isSubmitted, setIsSubmitted] = useState(false)
 
   return (
     <div className="fixed w-screen h-screen bg-cover bg-bottom bg-fog">
@@ -17,6 +19,7 @@ export default function Home() {
           <h2 className="text-2xl md:text-3xl lg:text-4xl font-main-bold text-tint-900 mb-4 text-center">
             🌱🎒🧵🌿🌞🏕️🐌 <br /> Walden Designs
           </h2>
+          {!isSubmitted ? 
           <form className="space-y-8 flex flex-col justify-center" onSubmit={(event) => handleSubmit(event)} data-netlify="true">
             <p className="text-tint-900 text-sm md:text-md lg:text-lg text-wrap">
               Sign up for our email list for notifications on launch.
@@ -26,7 +29,11 @@ export default function Home() {
               placeholder='Email address'
             />
             <Button onClick={(event) => handleSubmit(event)}>Sign up</Button>
-          </form>
+          </form> :
+          <div className="space-y-8 flex flex-col justify-center">
+            <h1 className="text-lg md:text-xl lg:text-2xl text-center">You&apos;re on the list!</h1>
+            <p className="text-sm md:text-md lg:text-lg text-center text-wrap">We&apos;ll keep you posted on what&apos;s in the works.</p>
+          </div>}
         </div>
       </div>
     </div>
