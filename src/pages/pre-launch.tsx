8518a0788d844e0f0c2c3f99dffcffc5ad507201
mroycec <mroycec@icloud.com>
2023-07-25 16:26:45 -0700

    Add next/head

--- src/pages/pre-launch.tsx
+++ src/pages/pre-launch.tsx
@@ -2,35 +2,41 @@
 import { ChangeEvent, SetStateAction, useState } from 'react'
 import Button from '../components/button/button'
 import Input from '../components/input/input'
+import Head from 'next/head'
 
 export default function Home() {
   const [email, setEmail] = useState('')
 
   return (
-    <div className="fixed w-screen h-screen bg-cover bg-bottom bg-fog">
-      <div className="fixed z-50 inset-0 flex items-center justify-center space-y-4">
-        <div className="bg-tint-100 p-8 rounded-lg shadow-lg">
-          <h2 className="text-2xl md:text-3xl lg:text-4xl font-main-bold text-tint-900 mb-4 text-center">
-            🌱🎒🧵🌿🌞🏕️🐌 <br /> Walden Designs
-          </h2>
-          <form className="space-y-8 flex flex-col justify-center" data-netlify="true" name="email-list" method="POST" action="/confirmation">
-            <input type="hidden" name="form-name" value="email-list" />
-            <p className="text-tint-900 text-sm md:text-md lg:text-lg text-wrap">
-              Sign up for our list for notifications on launch.
-            </p>
-            <Input
-              form="email-list"
-              type="email"
-              name="email"
-              label="Email"
-              placeholder='Email'
-              value={email}
-              setValue={setEmail}
-              />
-            <Button type="submit" id="sign up" disabled={!email}>Sign up</Button>
-          </form>
+    <main>
+      <Head>
+        <title>Walden Designs 🐌</title>
+        <meta name="description" content="Walden Design pre-launch site" />
+      </Head>
+      <div className="fixed w-screen h-screen bg-cover bg-bottom bg-fog">
+        <div className="fixed z-50 inset-0 flex items-center justify-center space-y-4">
+          <div className="bg-tint-100 p-8 rounded-lg shadow-lg">
+            <h2 className="text-2xl md:text-3xl lg:text-4xl font-main-bold text-tint-900 mb-4 text-center">
+              🌱🎒🧵🌿🌞🏕️🐌 <br /> Walden Designs
+            </h2>
+            <form className="space-y-8 flex flex-col justify-center" data-netlify="true" name="email-list" method="POST" action="/confirmation">
+              <input type="hidden" name="form-name" value="email-list" />
+              <p className="text-tint-900 text-sm md:text-md lg:text-lg text-wrap">
+                Sign up for our list for notifications on launch.
+              </p>
+              <Input
+                form="email-list"
+                type="email"
+                name="email"
+                label="Email"
+                placeholder='Email'
+                value={email}
+                setValue={setEmail} />
+              <Button type="submit" id="sign up" disabled={!email}>Sign up</Button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </main>
   )
 }
